bbc12c5abae9d8288ffc803df4235e6bf5ee8318
AyoubGharbi <contact@ayoub-gharbi.org>
2020-09-26 12:23:55 +0100

    #2 Control Sight Detection

---
+++ Assets/DaysGoneLookInteraction/Scripts/PlayerInteraction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.blue;
+		Gizmos.DrawLine(transform.position,
+					    transform.position + transform.forward * 3);
+	}
+}
